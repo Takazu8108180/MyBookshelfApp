c17f550bdcc1867d087d949a5c8cb1e741e33879
koichi.takato <koichi.takato@g.softbank.co.jp>
2022-12-30 23:54:21 +0900

    add: backend package

---
+++ backend/src/main/java/jp/personal/takaichi/MyBookself/MyBookselfApplication.java
@@ -0,0 +1,13 @@
+package jp.personal.takaichi.MyBookself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookselfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/jp/personal/takaichi/MyBookself/MyBookselfApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.personal.takaichi.MyBookself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
